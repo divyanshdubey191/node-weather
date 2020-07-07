744dc1ee997ce112aafe359e868047165b0258cd
Divyansh Dubey <divyanshdubey1998@gmail.com>
2020-07-08 01:19:11 +0530

    More weather info added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location', undefined)
         }
         else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out'+' There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out'+' There is a ' + body.currently.precipProbability + '% chance of rain. Humidity: '+ body.currently.humidity + ' Wind speed: ' + body.currently.windSpeed)
         }
     })
 }
